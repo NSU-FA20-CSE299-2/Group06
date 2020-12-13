277e2bb23949c9e01296088a37ce44c5f95d612f
Rifat Islam <rifat.islam173@northsouth.edu>
2020-12-13 23:36:16 +0600

    FacebookSDK code setup completed #15
    Testing required and code clean up required

--- Code/FacebookSDK/app/src/main/java/com/ece/nsu/fall2020/cse486/sec1/facebooksdk/MainActivity.java
+++ Code/FacebookSDK/app/src/main/java/com/ece/nsu/fall2020/cse486/sec1/facebooksdk/MainActivity.java
@@ -3,12 +3,46 @@ package com.ece.nsu.fall2020.cse486.sec1.facebooksdk;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView authIdTV;
+    LoginButton loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        authIdTV = findViewById(R.id.fbauthid);
+        loginButton = findViewById(R.id.loginBtn);
+
+        CallbackManager callbackManager = CallbackManager.Factory.create();
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                authIdTV.setText(getString(R.string.id).concat(loginResult.getAccessToken().getUserId()));
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(MainActivity.this, "Cancelled!", Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Toast.makeText(MainActivity.this, "Error Occurred!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        
     }
 }
