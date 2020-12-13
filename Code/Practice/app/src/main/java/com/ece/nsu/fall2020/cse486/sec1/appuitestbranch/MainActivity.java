aeb94751dde1b4e94bb6387e6f97892037d25e04
Rifat Islam <rifat.islam173@northsouth.edu>
2020-12-13 20:53:15 +0600

    AppUITestBranch created and added in code practice folder #7
    App UI will be designed in this project

---
+++ Code/Practice/app/src/main/java/com/ece/nsu/fall2020/cse486/sec1/appuitestbranch/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ece.nsu.fall2020.cse486.sec1.appuitestbranch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
