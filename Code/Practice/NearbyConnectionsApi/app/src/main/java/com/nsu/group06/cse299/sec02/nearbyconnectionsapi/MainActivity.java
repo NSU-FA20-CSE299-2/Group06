6d9ca90c26ac707f6096dc6629198378a897b252
ferdouszislam <ferdouszislam@gmail.com>
2020-12-16 12:19:08 +0600

    Enhancement #5: Hollow project created Hollow android studio project created to implement Nearby Connections Api Adapter.

---
+++ Code/Practice/NearbyConnectionsApi/app/src/main/java/com/nsu/group06/cse299/sec02/nearbyconnectionsapi/MainActivity.java
@@ -0,0 +1,13 @@
+package com.nsu.group06.cse299.sec02.nearbyconnectionsapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
