123212530bc6c5c0c584aeffbf7cbd76f6dfe22c
Rifat Islam <rifat.islam173@northsouth.edu>
2020-12-19 19:40:29 +0600

    Enhancement #7: AppUITest project space recreated in code/practice
    Previously deleted for cleanup.

---
+++ Code/Practice/AppUITest/app/src/main/java/com/ece/nsu/fall2020/cse299/sec2/appuitest/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ece.nsu.fall2020.cse299.sec2.appuitest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
