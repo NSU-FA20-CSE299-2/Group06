292263bc7e6df3fcaa78e4cfadda62fddda28fb6
ferdouszislam <ferdouszislam@gmail.com>
2020-12-21 01:11:00 +0600

    Enhancement #8: Login Activity UI created Demo UI for testing backend login code created. The UI will be edited later to be similar as the prototype.

---
+++ Code/MainProject/HelpMeApp/app/src/main/java/com/nsu/group06/cse299/sec02/helpmeapp/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.nsu.group06.cse299.sec02.helpmeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void loginClick(View view) {
+    }
+
+    public void dontHaveAccountClick(View view) {
+    }
+}
