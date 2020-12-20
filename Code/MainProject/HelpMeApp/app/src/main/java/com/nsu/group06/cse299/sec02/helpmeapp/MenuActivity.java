ebdcdc767b85287fcc2c8bc252547b25b626092d
ferdouszislam <ferdouszislam@gmail.com>
2020-12-21 02:05:58 +0600

    Enhancement #8: Menu Activity UI created Demo UI for testing user logged in state save code created. The UI will be edited later to be similar as the prototype.

---
+++ Code/MainProject/HelpMeApp/app/src/main/java/com/nsu/group06/cse299/sec02/helpmeapp/MenuActivity.java
@@ -0,0 +1,18 @@
+package com.nsu.group06.cse299.sec02.helpmeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+
+    public void logoutClick(View view) {
+    }
+}
