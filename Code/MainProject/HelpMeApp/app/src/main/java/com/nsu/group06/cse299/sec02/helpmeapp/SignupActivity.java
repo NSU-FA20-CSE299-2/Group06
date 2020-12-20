ae7d30e2f77f2c65e2c25d994b05fc9cb692e868
ferdouszislam <ferdouszislam@gmail.com>
2020-12-21 01:03:08 +0600

    Enhancement #8: Signup Activity UI created Demo UI for testing backend signup code created. The UI will be edited later to be similar as the prototype.

---
+++ Code/MainProject/HelpMeApp/app/src/main/java/com/nsu/group06/cse299/sec02/helpmeapp/SignupActivity.java
@@ -0,0 +1,21 @@
+package com.nsu.group06.cse299.sec02.helpmeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+
+    public void signupClick(View view) {
+    }
+
+    public void alreadyHaveAccountClick(View view) {
+    }
+}
