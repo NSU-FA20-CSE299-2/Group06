a9fc440eaa0392c19b059e59e3616db92299fef5
Rifat Islam <rifat.islam173@northsouth.edu>
2020-12-23 23:50:29 +0600

    Enhancement #26 : Setup Profile page designed, will be added in main project
    Resolve issue: #26

--- Code/Practice/AppUITest/app/src/main/java/com/ece/nsu/fall2020/cse299/sec2/appuitest/MainActivity.java
+++ Code/Practice/AppUITest/app/src/main/java/com/ece/nsu/fall2020/cse299/sec2/appuitest/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu_new);
+        setContentView(R.layout.activity_setup_profile);
     }
 }
