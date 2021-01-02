f2eb5f4347ba57780df99db04a1107aa303caf9f
ferdouszislam <ferdouszislam@gmail.com>
2021-01-02 21:02:25 +0600

    Enhancement #10: Emergency Contacts XML started.

---
+++ Code/MainProject/HelpMeApp/app/src/main/java/com/nsu/group06/cse299/sec02/helpmeapp/EmergencyContactsActivity.java
@@ -0,0 +1,21 @@
+package com.nsu.group06.cse299.sec02.helpmeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class EmergencyContactsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emergency_contacts);
+    }
+
+    /*
+    Add new emergency contact click
+     */
+    public void addClick(View view) {
+    }
+}
