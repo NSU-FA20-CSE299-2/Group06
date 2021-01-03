0cd0c35c331442ce9a9ef5488b426a9fa7b3b246
ferdouszislam <ferdouszislam@gmail.com>
2021-01-03 21:49:36 +0600

    Enhancement #10: User info read from database
    Read userinfo from database and put in the UI.

--- Code/MainProject/HelpMeApp/app/src/main/java/com/nsu/group06/cse299/sec02/helpmeapp/SetupProfileActivity.java
+++ Code/MainProject/HelpMeApp/app/src/main/java/com/nsu/group06/cse299/sec02/helpmeapp/SetupProfileActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -11,16 +12,29 @@ import android.widget.Toast;
 import com.nsu.group06.cse299.sec02.helpmeapp.auth.Authentication;
 import com.nsu.group06.cse299.sec02.helpmeapp.auth.AuthenticationUser;
 import com.nsu.group06.cse299.sec02.helpmeapp.auth.FirebaseEmailPasswordAuthentication;
+import com.nsu.group06.cse299.sec02.helpmeapp.database.Database;
+import com.nsu.group06.cse299.sec02.helpmeapp.database.firebase_database.FirebaseRDBApiEndPoint;
+import com.nsu.group06.cse299.sec02.helpmeapp.database.firebase_database.FirebaseRDBSingleOperation;
+import com.nsu.group06.cse299.sec02.helpmeapp.models.User;
+import com.nsu.group06.cse299.sec02.helpmeapp.utils.NosqlDatabasePathUtils;
 import com.nsu.group06.cse299.sec02.helpmeapp.utils.SessionUtils;
 
 public class SetupProfileActivity extends AppCompatActivity {
 
+    private static final String TAG = "SPA-debug";
+
     // ui
     private EditText mUsernameEditText, mDateOfBirhtEditText, mAddressEditText, mPhoneNumberEditText;
 
-    // variables used for fetching user uid
-    private AuthenticationUser mAuthenticationUser;
+    // model
+    private User mUser;
+
+    // variable used for fetching user uid
     private Authentication mAuth;
+
+    // variables to read existing information of users from the database
+    private Database.SingleOperationDatabase<User> mReadUserInfoFirebaseRDBSingleOperation;
+    private FirebaseRDBApiEndPoint mReadUserInfoApiEndPoint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,12 +51,14 @@ public class SetupProfileActivity extends AppCompatActivity {
         mAddressEditText = findViewById(R.id.address_setupProfile_EditText);
         mPhoneNumberEditText = findViewById(R.id.phoneNumber_setupProfile_EditText);
 
+        mUser = new User();
+
         // authenticate user, because we need uid here
         mAuth = new FirebaseEmailPasswordAuthentication(new Authentication.AuthenticationCallbacks() {
             @Override
             public void onAuthenticationSuccess(AuthenticationUser user) {
 
-                mAuthenticationUser = user;
+                mUser.setmUid(user.getmUid());
 
                 loadUserProfileInformation();
             }
@@ -61,6 +77,55 @@ public class SetupProfileActivity extends AppCompatActivity {
      */
     private void loadUserProfileInformation() {
         //TODO: implement
+
+        mReadUserInfoApiEndPoint =
+                new FirebaseRDBApiEndPoint(
+                        "/"+ NosqlDatabasePathUtils.USER_NODE +
+                                ":" + mUser.getmUid()
+                );
+
+        mReadUserInfoFirebaseRDBSingleOperation =
+                new FirebaseRDBSingleOperation(
+
+                        User.class,
+
+                        mReadUserInfoApiEndPoint,
+
+                        new Database.SingleOperationDatabase.SingleOperationDatabaseCallback<User>() {
+                            @Override
+                            public void onDataRead(User data) {
+
+                                mUser = data;
+                                showReadUserInfoInUI();
+                            }
+
+                            @Override
+                            public void onDatabaseOperationSuccess() {
+                                // not required
+                            }
+
+                            @Override
+                            public void onDatabaseOperationFailed(String message) {
+
+                                showToast(getString(R.string.failed_to_connect));
+
+                                Log.d(TAG, "onDatabaseOperationFailed: user data read error -> "+message);
+                            }
+                        }
+
+                );
+        mReadUserInfoFirebaseRDBSingleOperation.read();
+    }
+
+    /*
+      Show read user info from database to UI
+     */
+    private void showReadUserInfoInUI() {
+
+        mUsernameEditText.setText(mUser.getmUsername());
+        mDateOfBirhtEditText.setText(mUser.getmDateOfBirth());
+        mAddressEditText.setText(mUser.getmAddress());
+        mPhoneNumberEditText.setText(mUser.getmPhoneNumber());
     }
 
 
--- Code/MainProject/HelpMeApp/app/src/main/java/com/nsu/group06/cse299/sec02/helpmeapp/database/firebase_database/FirebaseRDBApiEndPoint.java
+++ Code/MainProject/HelpMeApp/app/src/main/java/com/nsu/group06/cse299/sec02/helpmeapp/database/firebase_database/FirebaseRDBApiEndPoint.java
@@ -77,6 +77,15 @@ public class FirebaseRDBApiEndPoint extends ApiEndPoint<Query> {
             query = firebaseDatabase.getReference().child(NosqlDatabasePathUtils.USER_NODE);
         }
 
+        // reference to the whole "users:userId" node
+        else if(mUrl.startsWith("/"+NosqlDatabasePathUtils.USER_NODE+":")){
+
+            String uid = mUrl.substring(mUrl.lastIndexOf(':')+1).trim();
+
+            query = firebaseDatabase.getReference().child(NosqlDatabasePathUtils.USER_NODE)
+                    .orderByKey().equalTo(uid);
+        }
+
         // reference to "emergencyContacts/uid/phoneNumbers"
         else if(mUrl.startsWith("/" + NosqlDatabasePathUtils.EMERGENCY_CONTACTS_NODE)
                 && mUrl.endsWith("/" + NosqlDatabasePathUtils.EMERGENCY_CONTACTS_PHONE_NODE)){
