413a1cde754cfa0719f23a4e98f4e134403058da
Rifat Islam <rifat.islam173@northsouth.edu>
2021-01-08 18:14:40 +0600

    Enhancement #13: Help post route seperated. Views folder restructed.

---
+++ Code/MainProject/WebApp/help.js
@@ -0,0 +1,30 @@
+const { default: axios } = require('axios')
+const express = require('express')
+const router = express.Router()
+const path = require('path')
+
+router.get('/', (req,res,next) =>{
+    axios({
+        method: 'get',
+        url: 'https://helpmeapi-deploy.herokuapp.com/post',
+        params: {
+            uid: "8989898989",
+            pid: "Xtsd13Axd123zXc"
+        }
+    }).then(response => {
+        console.log(response.data)
+        const post = response.data
+        if (post.result == false) {
+            res.render('errorpage');
+        }
+        else {
+            const time = "Confused Unga Bunga" //timeStamp will be converted to real time and date
+            const date = "Also Confused Unga Bunga"
+            res.render('index', { data: post, time: time, date: date })
+        }
+    })
+        .catch(err => console.error(err))
+
+})
+
+module.exports = router
--- Code/MainProject/WebApp/index.js
+++ Code/MainProject/WebApp/index.js
@@ -1,40 +1,22 @@
 const { default: axios } = require('axios')
 const express = require('express')
 const app = express()
+const postRoute = require('./help')
 const path = require('path')
 const port = 3000
 
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views/pages'));
 app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/views'));
 
+//help post route
+app.use('/post', postRoute)
+
+//home page router
 app.get('/', (req, res) => {
-
     //console.log(req.query.id,req.query.pid)
-    
-    axios({
-        method: 'get',
-        url:'https://helpmeapi-deploy.herokuapp.com/post',
-        params : {
-            uid: "8989898989",
-            pid: "Xtsd13Axd123zXc"
-        }
-    }).then(response => {        
-        console.log(response.data)
-        const post = response.data
-        if(post.result == false)
-        {
-            res.render('errorpage');
-        }
-        else{
-            const time = "Confused Unga Bunga" //timeStamp will be converted to real time and date
-            const date = "Also Confused Unga Bunga"
-            res.render('index', { data: post, time: time ,date: date})
-        }
-    })
-    .catch(err => console.error(err))
+    res.send("App Lander")
 })
-
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
