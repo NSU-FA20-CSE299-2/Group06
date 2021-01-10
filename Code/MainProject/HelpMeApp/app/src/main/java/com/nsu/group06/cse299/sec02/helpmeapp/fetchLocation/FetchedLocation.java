c5c7555ea3164789bc046326bac1cc59e0ed727b
ferdouszislam <ferdouszislam@gmail.com>
2021-01-11 00:43:01 +0600

    Enhancement #11: Location fetching classes imported into Main app
    The classes were copied from Code/Practice/FusedLocationAPIAdapter

---
+++ Code/MainProject/HelpMeApp/app/src/main/java/com/nsu/group06/cse299/sec02/helpmeapp/fetchLocation/FetchedLocation.java
@@ -0,0 +1,133 @@
+package com.nsu.group06.cse299.sec02.helpmeapp.fetchLocation;
+
+/*
+model for fetched location data
+ */
+public class FetchedLocation {
+
+    private double mLatitude, mLongitude, mAltitude, mAccuracy;
+    private String mAddress = "";
+
+    // TODO: calculate this value properly
+    private static final double SIGNIFICANT_DIFFERENCE_DISTANCE = 20.00d; // in meters
+
+    public FetchedLocation() {
+    }
+
+    public FetchedLocation(double mLatitude, double mLongitude) {
+        this.mLatitude = mLatitude;
+        this.mLongitude = mLongitude;
+    }
+
+    public FetchedLocation(double mLatitude, double mLongitude, double mAltitude, double mAccuracy) {
+        this.mLatitude = mLatitude;
+        this.mLongitude = mLongitude;
+        this.mAltitude = mAltitude;
+        this.mAccuracy = mAccuracy;
+    }
+
+    public FetchedLocation(double mLatitude, double mLongitude, double mAltitude, double mAccuracy, String mAddress) {
+        this.mLatitude = mLatitude;
+        this.mLongitude = mLongitude;
+        this.mAltitude = mAltitude;
+        this.mAccuracy = mAccuracy;
+        this.mAddress = mAddress;
+    }
+
+    /*
+    Return true if there is significant difference between the two locations
+    i.e distance between them is more than SIGNIFICANT_DIFFERENCE_DISTANCE(= 20 meters)
+     */
+    public static boolean isLocationSignificantlyDifferent(FetchedLocation location1, FetchedLocation location2){
+
+        return distanceBetween(location1.getmLatitude(), location2.getmLatitude(), location1.getmLongitude(), location2.getmLongitude())
+                >= SIGNIFICANT_DIFFERENCE_DISTANCE;
+    }
+
+    /*
+    Returns distance in meters between two latLng points
+    courtesy- <https://www.geeksforgeeks.org/program-distance-two-points-earth/>
+     */
+    private static double distanceBetween(double lat1, double lat2, double lon1, double lon2)
+    {
+
+        // The math module contains a function
+        // named toRadians which converts from
+        // degrees to radians.
+        lon1 = Math.toRadians(lon1);
+        lon2 = Math.toRadians(lon2);
+        lat1 = Math.toRadians(lat1);
+        lat2 = Math.toRadians(lat2);
+
+        // Haversine formula
+        double dlon = lon2 - lon1;
+        double dlat = lat2 - lat1;
+        double a = Math.pow(Math.sin(dlat / 2), 2)
+                + Math.cos(lat1) * Math.cos(lat2)
+                * Math.pow(Math.sin(dlon / 2),2);
+
+        double c = 2 * Math.asin(Math.sqrt(a));
+
+        // Radius of earth in meters.
+        double r = 6371*1000;
+
+        // calculate the result
+        return(c * r);
+    }
+
+    public double getmLatitude() {
+        return mLatitude;
+    }
+
+    public void setmLatitude(double mLatitude) {
+        this.mLatitude = mLatitude;
+    }
+
+    public double getmLongitude() {
+        return mLongitude;
+    }
+
+    public void setmLongitude(double mLongitude) {
+        this.mLongitude = mLongitude;
+    }
+
+    public double getmAltitude() {
+        return mAltitude;
+    }
+
+    public void setmAltitude(double mAltitude) {
+        this.mAltitude = mAltitude;
+    }
+
+    public double getmAccuracy() {
+        return mAccuracy;
+    }
+
+    public void setmAccuracy(double mAccuracy) {
+        this.mAccuracy = mAccuracy;
+    }
+
+    public String getmAddress() {
+        return mAddress;
+    }
+
+    public void setmAddress(String mAddress) {
+        this.mAddress = mAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "FetchedLocation{\n" +
+                "mLatitude=" +  roundTwoDecimal(mLatitude) +
+                ", mLongitude=" + roundTwoDecimal(mLongitude) +
+                ", mAltitude=" + roundTwoDecimal(mAltitude) +
+                ", mAccuracy=" + roundTwoDecimal(mAccuracy) +
+                ",\nmAddress=" + mAddress +
+                '}';
+    }
+
+    private String roundTwoDecimal(double d){
+
+        return String.format("%.2f", d);
+    }
+}
---
+++ Code/MainProject/HelpMeApp/app/src/main/java/com/nsu/group06/cse299/sec02/helpmeapp/fetchLocation/fusedLocationApi/FusedLocationFetcherApiAdapter.java
@@ -0,0 +1,138 @@
+package com.nsu.group06.cse299.sec02.helpmeapp.fetchLocation.fusedLocationApi;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Looper;
+import android.util.Log;
+
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.Task;
+import com.nsu.group06.cse299.sec02.helpmeapp.fetchLocation.FetchedLocation;
+import com.nsu.group06.cse299.sec02.helpmeapp.fetchLocation.LocationFetcher;
+
+/**
+ * Api adapter for fused locations api
+ */
+public class FusedLocationFetcherApiAdapter extends LocationFetcher {
+
+    private static final String TAG = "FLFAA-debug";
+
+    // location access priority
+    public static int PRIORITY_LEVEL = LocationRequest.PRIORITY_HIGH_ACCURACY;
+
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private LocationRequest mLocationRequest;
+    private LocationCallback mLocationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            if (locationResult == null) {
+                return;
+            }
+            for (Location location : locationResult.getLocations()) {
+
+                FetchedLocation fetchedLocation = new FetchedLocation(
+                        location.getLatitude(), location.getLongitude(),
+                        location.getAltitude(), location.getAccuracy());
+
+                locationUpdateListener.onNewLocationUpdate(fetchedLocation);
+            }
+        }
+
+        @Override
+        public void onLocationAvailability(LocationAvailability locationAvailability) {
+
+            if(!locationAvailability.isLocationAvailable()) {
+
+                locationUpdateListener.onError("location not available");
+                Log.d(TAG, "onLocationAvailability: location not available");
+            }
+
+            super.onLocationAvailability(locationAvailability);
+        }
+    };
+
+
+    public FusedLocationFetcherApiAdapter(long interval, Context context, LocationSettingsSetupListener locationSettingsSetupListener,
+                                          LocationUpdateListener locationUpdateListener) {
+        super(interval, context, locationSettingsSetupListener, locationUpdateListener);
+
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setInterval(super.interval);
+        mLocationRequest.setFastestInterval(super.interval / 2);
+        mLocationRequest.setPriority(PRIORITY_LEVEL);
+
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context);
+    }
+
+    @Override
+    public void setupLocationSettings(Activity activity) {
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(mLocationRequest);
+
+        SettingsClient client = LocationServices.getSettingsClient(activity);
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+
+        task.addOnSuccessListener(activity, locationSettingsResponse -> {
+            // All location settings are satisfied. The client can initialize
+            // location requests here.
+
+            locationSettingsSetupListener.onLocationSettingsSetupSuccess();
+        });
+
+        task.addOnFailureListener(activity, e -> {
+
+            locationSettingsSetupListener.onLocationSettingsSetupFailed("location settings not met");
+
+            if (e instanceof ResolvableApiException) {
+                // Location settings are not satisfied, but this can be fixed
+                // by showing the user a dialog.
+                try {
+                    // Show the dialog by calling startResolutionForResult(),
+                    // and check the result in onActivityResult().
+                    ResolvableApiException resolvable = (ResolvableApiException) e;
+                    resolvable.startResolutionForResult(activity,
+                            REQUEST_CHECK_SETTINGS);
+                } catch (IntentSender.SendIntentException sendEx) {
+                    // Ignore the error.
+                }
+            }
+        });
+    }
+
+    @Override
+    public void startLocationUpdate() {
+
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            locationUpdateListener.onPermissionNotGranted();
+            return;
+        }
+
+        mFusedLocationProviderClient
+                .requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.getMainLooper());
+    }
+
+    @Override
+    public void stopLocationUpdate() {
+
+        mFusedLocationProviderClient
+                .removeLocationUpdates(mLocationCallback);
+    }
+}
