7184af1f1567bf45b80c116fa81fdd88cfbbccee
ferdouszislam <ferdouszislam@gmail.com>
2021-01-12 04:52:48 +0600

    Enhancement #11: Single help post loaded from URL except image
    help post data load from database for postId provided in the URL parameter done.

--- Code/MainProject/HelpMeApp/app/src/main/java/com/nsu/group06/cse299/sec02/helpmeapp/SingleHelpPostActivity.java
+++ Code/MainProject/HelpMeApp/app/src/main/java/com/nsu/group06/cse299/sec02/helpmeapp/SingleHelpPostActivity.java
@@ -10,6 +10,12 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.nsu.group06.cse299.sec02.helpmeapp.database.Database;
+import com.nsu.group06.cse299.sec02.helpmeapp.database.firebase_database.FirebaseRDBApiEndPoint;
+import com.nsu.group06.cse299.sec02.helpmeapp.database.firebase_database.FirebaseRDBSingleOperation;
+import com.nsu.group06.cse299.sec02.helpmeapp.models.HelpPost;
+import com.nsu.group06.cse299.sec02.helpmeapp.utils.NosqlDatabasePathUtils;
+
 /**
  * Activity for showing a single help post
  * this activity is linked to URL received by emergency contacts through SMS
@@ -24,12 +30,43 @@ public class SingleHelpPostActivity extends AppCompatActivity {
     private TextView mContentTextView, mSearchingTextView;
     private ImageView mPhotoImageView;
 
+    // model
+    private HelpPost mHelpPost;
+    private String mRetrievedPostId;
+
+    // variables to read the help post from database
+    private Database.SingleOperationDatabase<HelpPost> mReadHelpPostSingleOperationDatabase;
+    private FirebaseRDBApiEndPoint mApiEndPoint;
+    private Database.SingleOperationDatabase.SingleOperationDatabaseCallback<HelpPost> mReadHelpPostSingleOperationDatabaseCallback =
+            new Database.SingleOperationDatabase.SingleOperationDatabaseCallback<HelpPost>() {
+                @Override
+                public void onDataRead(HelpPost data) {
+
+                    mHelpPost = data;
+                    showHelpPostUI(mHelpPost);
+                }
+
+                @Override
+                public void onDatabaseOperationSuccess() {
+                    // kept blank intentionally
+                }
+
+                @Override
+                public void onDatabaseOperationFailed(String message) {
+
+                    failedToLoadHelpPostUI();
+
+                    Log.d(TAG, "onDatabaseOperationFailed: help post read error -> "+message);
+                }
+            };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_help_post);
 
-        handleOpenFromURLIntent(getIntent());
+        handleIntent(getIntent());
 
         init();
     }
@@ -48,44 +85,82 @@ public class SingleHelpPostActivity extends AppCompatActivity {
 
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
-        handleOpenFromURLIntent(intent);
+        handleIntent(intent);
     }
 
-    private void handleOpenFromURLIntent(Intent intent) {
+    private void handleIntent(Intent intent) {
 
         String appLinkAction = intent.getAction();
-
         Uri appLinkData = intent.getData();
+
+        String pid = "";
 
         if (Intent.ACTION_VIEW.equals(appLinkAction) && appLinkData != null){
 
-            /*
-            String recipeId = appLinkData.getLastPathSegment();
-            Uri appData = Uri.parse("content://com.recipe_app/recipe/").buildUpon()
-                    .appendPath(recipeId).build();
-            */
-
-            showHelpPost(appLinkData);
+            pid = appLinkData.getQueryParameter("pid");
         }
+
+        mRetrievedPostId = pid;
+        loadHelpPost(mRetrievedPostId);
     }
 
-    private void showHelpPost(Uri appData) {
+    /**
+     * start fetching the help post from database
+     * @param postId ID of the post to be shown
+     */
+    private void loadHelpPost(String postId) {
 
-        Log.d(TAG, "showHelpPost: pid = "+appData.getQueryParameter("pid"));
+        mApiEndPoint = new FirebaseRDBApiEndPoint("/"+ NosqlDatabasePathUtils.HELP_POSTS_NODE+"/post:"+postId);
+
+        mReadHelpPostSingleOperationDatabase = new FirebaseRDBSingleOperation<HelpPost>(
+                HelpPost.class,
+                mApiEndPoint,
+                mReadHelpPostSingleOperationDatabaseCallback
+        );
+
+        mReadHelpPostSingleOperationDatabase.readSingle();
     }
 
+    /**
+     * show help post data to user
+     * @param helpPost post model object to be shown
+     */
+    private void showHelpPostUI(HelpPost helpPost) {
+
+        mSearchingTextView.setVisibility(View.GONE);
+
+        main_layout.setVisibility(View.VISIBLE);
+
+        mPhoneNumberTextView.setText(helpPost.getAuthorPhoneNumber());
+
+        if( helpPost.getAddress() !=null && !helpPost.getAddress().isEmpty()) mAddressTextView.setText(helpPost.getAddress());
+        else mAddressTextView.setText(R.string.no_address);
+
+        mTimeTextView.setText(mHelpPost.getTimeStamp());
+
+        mContentTextView.setText(mHelpPost.getContent());
+
+    }
 
     /*
     "Contact" button click listener
      */
     public void contactClick(View view) {
-
-
     }
 
     /*
     "Show Location" button click listener
      */
     public void showLocationClick(View view) {
+    }
+
+    private void failedToLoadHelpPostUI() {
+
+        mSearchingTextView.setText(R.string.failed_to_load_post);
+    }
+
+    public void retryClick(View view) {
+
+        loadHelpPost(mRetrievedPostId);
     }
 }
