1c48ebde6bc57c425ee3dd3107c5632b5ea164f1
Rifat Islam <rifat.islam173@northsouth.edu>
2021-01-12 02:05:03 +0600

    Enhancement #13: server.js updated in web-api project
    Api query parameter changed (removed uid param)

--- Code/MainProject/WebApi/server.js
+++ Code/MainProject/WebApi/server.js
@@ -5,7 +5,8 @@ const PORT = process.env.PORT || 3000
 const admin = require('firebase-admin')
 var Firebase = require('firebase');
 const fs = require('fs');
-
+const { default: axios } = require('axios')
+const geoCodingRoute = require('./geocoding')
 
 
 var serviceAccount = require("./api-testing-b6ee5-firebase-adminsdk-zu5i1-53b9ce1a66.json");
@@ -20,48 +21,49 @@ app.use(function (req, res, next) { //allow cross origin requests
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: "PLACE DATABASE URL HERE"
+    databaseURL: "https://api-testing-b6ee5-default-rtdb.firebaseio.com"
 });
 
 Firebase.initializeApp({
 
-    databaseURL: "PLACE DATABASE URL HERE",
-    serviceAccount: serviceAccount, 
+    databaseURL: "https://api-testing-b6ee5-default-rtdb.firebaseio.com/",
+
+    serviceAccount: serviceAccount, //downloaded from Firebase Console
+
 });
 
+app.use('/geocode', geoCodingRoute)
 
+
+// app.use('/', postRouter)
 var db = Firebase.database();
-var usersRef = db.ref("posts");
+var usersRef = db.ref("helpPosts");
 
-//For viewing post by UID and PID(user)
+
+//For viewing post by PID(user)
 app.get('/post', function (req, res) {
 
-    const uid = req.query.uid
     const pid = req.query.pid
-
     let date_ob = new Date();
     let logdata = {
         time: date_ob.toLocaleTimeString(),
         date: date_ob.toLocaleDateString(),
-        uid: uid,
         pid: pid,
-        request: "user query by uid & pid",
+        request: "user query by pid",
         status: "success"
     }
 
-    if (uid == null || pid == null) { //
-
+    if ( pid == null) { //
         logdata.status = "failed"
         res.json({ message: "Error: Invalid query parameter.", result: false });
-
     }
-    else if (uid.length < 5 || pid.length < 5) //firebase uid length: 20
+    else if ( pid.length < 5) //firebase pid length: 20
     {
         logdata.status = "failed"
-        res.json({ message: "Error: Invalid UID/PID.", result: false });
+        res.json({ message: "Error: Invalid PID.", result: false });
     }
     else {
-        usersRef.child(uid).child(pid).once("value", function (snapshot) {
+        usersRef.child(pid).once("value", function (snapshot) {
             if (snapshot.val() == null) {
 
                 res.json({ message: "Error: No data found", "result": false });
@@ -73,78 +75,20 @@ app.get('/post', function (req, res) {
         });
     }
 
-
     fs.appendFile('logs.txt', JSON.stringify(logdata) + "\n", function (err) {
         if (err) throw err;
     });
 
 });
 
-//For viewing all posts by UID (user)
-app.get('/posts', function (req, res) {
-
-    const uid = req.query.uid
-
-    let date_ob = new Date();
-    let logdata = {
-        time: date_ob.toLocaleTimeString(),
-        date: date_ob.toLocaleDateString(),
-        uid: uid,
-        pid: pid,
-        request: "user query by uid",
-        status: "success"
-    }
-
-
-    if (uid == null) { //
-
-        logdata.status = "failed"
-        res.json({ message: "Error: Invalid query parameter.", result: false });
-
-    }
-    else if (uid.length < 5) //firebase uid length: 20
-    {
-        logdata.status = "failed"
-        res.json({ message: "Error: Invalid UID/PID.", result: false });
-    }
-    else {
-        usersRef.child(uid).once("value", function (snapshot) {
-            if (snapshot.val() == null) {
-
-                res.json({ message: "Error: No data found", "result": false });
-
-            } else {
-
-                res.json(snapshot.val());
-            }
-        });
-    }
-    fs.appendFile('logs.txt', JSON.stringify(logdata) + "\n", function (err) {
-        if (err) throw err;
-    });
-});
-
-
-//For viewing all posts (dev)
-app.get('/allPosts', function (req, res) {
-
-    usersRef.once("value", function (snapshot) {
-        if (snapshot.val() == null) {
-
-            res.json({ message: "Error: No data found", "result": false });
-
-        } else {
-
-            res.json(snapshot.val());
-        }
-    });
-});
 
 
 //API Home
-app.get('/', (req, res) => {
+app.get('/', (req,res)=>{
     res.send("<center><h1>Help Post API Home</h1></center>")
 })
+
+
 
 
 //View API Logs
@@ -152,10 +96,61 @@ app.get('/devlog', (req, res) => {
     fs.readFile('logs.txt', (e, data) => {
         if (e) throw e;
         let htmlHeader = "<h1>API Logs (Dev) </h1>\n"
-        res.send(htmlHeader + data.toString());
+        res.send(htmlHeader+data.toString());
     });
 })
 
+
+//Database trigger route
+app.get('/trigger', (req, res,next) => {
+    const pid = req.query.pid
+
+    if (pid == null || pid.length < 5 ) { //
+        res.json({ status:"FAILED",message: "Error: Invalid query parameter(pid => null/value too short).", result: false })
+        next()
+    }
+    else {
+        usersRef.child(pid).once("value", function (snapshot) {
+            if (snapshot.val() == null) {
+                res.json({ message: "Error: No data found", "result": false })
+                next()
+            } else {
+                const post = snapshot.val()
+                if (post.address == null || post.address == "") {
+                    var lat = post.latitude
+                    var long = post.longitude
+                    var qValueParam = lat + "+" + long
+                    var apiKeyValuParam = "edf1ffbd06444d9883e6005fdb80f7f3"                    
+                    axios.get(
+                         "https://api.opencagedata.com/geocode/v1/json?q=" + qValueParam + "&key=" + apiKeyValuParam
+                    ).then(response => {
+                        const apiData = response.data
+                        var address = "Cannot retrive address"
+                        if (apiData.results!=null)
+                        {
+                            address = apiData.results[0].formatted
+                        }
+                        usersRef.child(pid).update({ address:address})
+                        res.json({ status: "OK", message: "Address updated" })
+                        next()
+                    }).catch(err => {
+                        console.log(err)
+                        var address = "Cannot retrive address"
+                        usersRef.child(pid).update({ address: address })
+                        res.json({status:"FAILED", message:"Could not update address"})
+                        next()
+                    })
+                }
+                else
+                {
+                    res.json({ status: "FAILED", message: "Not requied" })
+                    next()
+                }
+            }
+        });
+    }
+
+})
 
 
 app.listen(PORT, ()=>console.log("Server is running on PORT " + PORT))
