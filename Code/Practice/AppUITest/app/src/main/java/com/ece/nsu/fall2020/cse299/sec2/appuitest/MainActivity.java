8f84ea17130d9fd1a5f89aab1624b126b0491d66
Rifat Islam <rifat.islam173@northsouth.edu>
2021-01-16 04:21:21 +0600

    Enhancement #36: Splash screen designed, tested and added in AppUITest project, will be implemented in main project

--- Code/Practice/AppUITest/app/src/main/java/com/ece/nsu/fall2020/cse299/sec2/appuitest/MainActivity.java
+++ Code/Practice/AppUITest/app/src/main/java/com/ece/nsu/fall2020/cse299/sec2/appuitest/MainActivity.java
@@ -1,8 +1,11 @@
 package com.ece.nsu.fall2020.cse299.sec2.appuitest;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.rbddevs.splashy.Splashy;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup_profile);
+        setSplashy();
+
+    }
+
+    public void setSplashy(){
+        new Splashy(this)
+                .setLogo(R.drawable.ic_applogo)
+                .setTitle("Help Me App")
+                .setTitleColor("#272727")
+                .setSubTitle("Stand against the social violence around you")
+                .setSubTitleColor("#374045")
+                .setBackgroundResource(R.drawable.custom_gradient_color)
+                .setProgressColor("#FFFFFF")
+                .setFullScreen(true)
+                .setDuration(3000)
+                .show();
     }
 }
