840c4562560dd13e7d749ad3c2602250b0d1d530
Rifat Islam <rifat.islam173@northsouth.edu>
2021-01-15 18:12:55 +0600

    Enhancement #13: Ajax route for empty address fetching added in index.js, configured and tested. Font-end js functions can send ajax request and get valid address as response.
    Empty addess problem fixed :+1:

--- Code/MainProject/WebApp/index.js
+++ Code/MainProject/WebApp/index.js
@@ -5,6 +5,7 @@ const postRoute = require('./help')
 const path = require('path')
 const port = process.env.PORT || 3000
 
+
 app.set('views', path.join(__dirname, 'views/pages'));
 app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/views'));
@@ -17,6 +18,25 @@ app.use('/help', postRoute)
 //home page router
 app.get('/', (req, res) => {
     res.render('index')
+})
+
+
+//ajax request handler
+app.get('/trigger', (req, res) => {
+  const pid = req.query.pid
+  axios({
+    method: 'get',
+    url: 'https://helpmeapi-deploy.herokuapp.com/trigger',
+    params: {pid: pid}
+  }).then(response =>
+    {
+      //console.log(response.data)
+      let addressData = response.data
+      if (addressData.status != "FAILED")
+      {
+        res.send(addressData.address);
+      }
+    })
 })
 
 
